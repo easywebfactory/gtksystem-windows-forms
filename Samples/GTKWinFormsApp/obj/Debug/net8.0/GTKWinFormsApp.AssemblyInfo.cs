ca6c110bb74e7bc9fbe23b899ee95d3d7e64eee5
chenhongjin <438865652@qq.com>
2024-01-24 20:29:03 +0800

    up

--- Samples/GTKWinFormsApp/obj/Debug/net8.0/GTKWinFormsApp.AssemblyInfo.cs
+++ Samples/GTKWinFormsApp/obj/Debug/net8.0/GTKWinFormsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GTKWinFormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcf77fbb5d847b4696fc4f775bb879870ccdcb3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GTKWinFormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GTKWinFormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
