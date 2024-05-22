902a0ac5fe19c4a1c73c72998cac38e51cf3fa06
chenhongjin <438865652@qq.com>
2024-05-22 16:32:06 +0800

    up

--- Samples/GTKWinFormsApp/Program.cs
+++ Samples/GTKWinFormsApp/Program.cs
@@ -15,14 +15,6 @@ namespace GTKWinFormsApp
         [STAThread]
         static void Main()
         {
-            Console.WriteLine("ExecutablePath:" + Application.ExecutablePath);
-            Console.WriteLine("ExecutablePath:" + System.Diagnostics.Process.GetCurrentProcess().MainModule.ModuleName);
-
-           // Console.WriteLine("CommonAppDataPath:" + Application.CommonAppDataPath);
-           // Console.WriteLine("UserAppDataPath:" + Application.UserAppDataPath);
-           // Console.WriteLine("LocalUserAppDataPath:" + Application.LocalUserAppDataPath);
-            Console.WriteLine("StartupPath:" + Application.StartupPath);
-            Console.WriteLine("UserAppDataPath:" + Application.UserAppDataPath);
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
