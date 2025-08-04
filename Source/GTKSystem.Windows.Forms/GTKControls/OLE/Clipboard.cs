a4ec4522851b70be4f0a41c3076e046f2b6af6d8
chenhongjin <13632417+easywebfactory@user.noreply.gitee.com>
2025-08-05 05:08:22 +0800

    增加clipboard

---
+++ Source/GTKSystem.Windows.Forms/GTKControls/OLE/Clipboard.cs
@@ -0,0 +1,190 @@
+﻿using Gtk;
+namespace System.Windows.Forms
+{
+
+    public static class Clipboard
+    {
+        public static void SetDataObject(object data) => SetDataObject(data, copy: false);
+
+        public static void SetDataObject(object data, bool copy) => SetDataObject(data, copy, retryTimes: 10, retryDelay: 100);
+
+        public static void SetDataObject(object data, bool copy, int retryTimes, int retryDelay)
+        {
+            DataObject dataObject = new DataObject();
+            dataObject.SetData(DataFormats.Serializable, true, data);
+        }
+
+        public static IDataObject? GetDataObject()
+        {
+            return new DataObject();
+        }
+
+        public static void Clear() => SetDataObject(new DataObject());
+
+        public static object? GetData(string format) => string.IsNullOrWhiteSpace(format) ? null : GetData(format, autoConvert: false);
+
+        private static object? GetData(string format, bool autoConvert)
+            => GetDataObject() is { } dataObject ? dataObject.GetData(format, autoConvert) : null;
+
+        public static Drawing.Image? GetImage() => GetData(DataFormats.Bitmap, autoConvert: true) as Drawing.Image;
+
+        public static string GetText() => GetText(TextDataFormat.UnicodeText);
+
+        public static string GetText(TextDataFormat format)
+        {
+            return GetData(format.ToString()) as string ?? string.Empty;
+        }
+        public static void SetData(string format, object data)
+        {
+            SetDataObject(new DataObject(format, data), copy: true);
+        }
+
+        public static void SetImage(Drawing.Image image) => SetDataObject(new DataObject(image), copy: true);
+
+        public static void SetText(string text) => SetText(text, TextDataFormat.UnicodeText);
+
+        public static void SetText(string text, TextDataFormat format)
+        {
+            var clipboard = Gtk.Clipboard.Get(Gdk.Selection.Clipboard);
+            clipboard.Text = text;
+            clipboard.Store();
+        }
+        public class ClipDataObject
+        {
+            public ClipDataObject(object obj)
+            {
+                Data = obj;
+            }
+            public object Data { get; set; }
+        }
+        public partial class DataObject : IDataObject
+        {
+            Gtk.Clipboard clipboard = Gtk.Clipboard.Get(Gdk.Selection.Clipboard);
+            public DataObject()
+            {
+
+            }
+            public DataObject(object data) : this(DataFormats.Serializable, data)
+            {
+
+            }
+            public DataObject(string format, object data) : this() => SetData(format, data);
+
+            internal DataObject(string format, bool autoConvert, object data) : this() => SetData(format, autoConvert, data);
+
+
+            public object GetData(string format, bool autoConvert)
+            {
+
+                if (format == DataFormats.Text || format == DataFormats.UnicodeText)
+                {
+                    string value = string.Empty;
+                    clipboard.RequestText((clip, text) =>
+                    {
+                        value = text;
+                    });
+                    return value;
+                }
+                else if (format == DataFormats.Bitmap)
+                {
+                    Drawing.Bitmap bitmap = null;
+                    clipboard.RequestImage((clip, image) =>
+                    {
+                        bitmap = new Drawing.Bitmap(image.Width, image.Height);
+                        bitmap.Pixbuf = image;
+                    });
+                    return bitmap;
+                }
+                else
+                {
+                    object result = null;
+                    clipboard.RequestContents(Gdk.Selection.Clipboard, new ClipboardReceivedFunc((clip, data) =>
+                    {
+                        using (MemoryStream ms = new MemoryStream(data.Data))
+                        {
+                            System.Runtime.Serialization.DataContractSerializer dataContract = new Runtime.Serialization.DataContractSerializer(typeof(ClipDataObject));
+                            ClipDataObject dataobj = dataContract.ReadObject(ms) as ClipDataObject;
+                            result = dataobj.Data;
+                        }
+                    }));
+                    return result;
+                }
+            }
+
+            public object GetData(string format)
+            {
+                return GetData(format, true);
+            }
+
+            public object GetData(Type format)
+            {
+                return GetData(format.Name, true);
+            }
+
+            public bool GetDataPresent(string format, bool autoConvert)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool GetDataPresent(string format)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool GetDataPresent(Type format)
+            {
+                throw new NotImplementedException();
+            }
+
+            public string[] GetFormats(bool autoConvert)
+            {
+                throw new NotImplementedException();
+            }
+
+            public string[] GetFormats()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void SetData(string format, bool autoConvert, object data)
+            {
+                if (format == DataFormats.Text || format == DataFormats.UnicodeText)
+                {
+                    clipboard.Text = data?.ToString() ?? string.Empty;
+                }
+                else if (format == DataFormats.Bitmap)
+                {
+                    clipboard.Image = (Gdk.Pixbuf)((Drawing.Image)data).Pixbuf.Clone();
+                }
+                else
+                {
+                    clipboard.SetWithData([new Gtk.TargetEntry(format, Gtk.TargetFlags.Widget, 0)], (clip, selectiondata, inf) =>
+                    {
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            System.Runtime.Serialization.DataContractSerializer dataContract = new Runtime.Serialization.DataContractSerializer(typeof(ClipDataObject));
+                            dataContract.WriteObject(ms, new ClipDataObject(data));
+                            selectiondata.Set(Gdk.Selection.Clipboard, 0, ms.ToArray());
+                        }
+                    }, new Gtk.ClipboardClearFunc(clip => { }));
+                    clipboard.Store();
+                }
+            }
+
+            public void SetData(string format, object data)
+            {
+                SetData(format, true, data);
+            }
+
+            public void SetData(Type format, object data)
+            {
+                SetData(format.Name, true, data);
+            }
+
+            public void SetData(object data)
+            {
+                SetData(DataFormats.Serializable, true, data);
+            }
+        }
+    }
+}
---
+++ Source/GTKSystem.Windows.Forms/GTKControls/OLE/DataFormats.cs
@@ -0,0 +1,146 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace System.Windows.Forms
+{
+    /// <summary>
+    ///  Translates between WinForms text-based <see cref="Clipboard"/>
+    ///  formats and Win32 32-bit signed integer-based clipboard
+    ///  formats. Provides <see langword="static"/> methods to create new
+    /// <see cref="Clipboard"/> formats and add them to the Windows Registry.
+    /// </summary>
+    public static partial class DataFormats
+    {
+        internal const string TextConstant = "Text";
+        internal const string UnicodeTextConstant = "UnicodeText";
+        internal const string DibConstant = "DeviceIndependentBitmap";
+        internal const string BitmapConstant = "Bitmap";
+        internal const string EmfConstant = "EnhancedMetafile";
+        internal const string WmfConstant = "MetaFilePict";
+        internal const string SymbolicLinkConstant = "SymbolicLink";
+        internal const string DifConstant = "DataInterchangeFormat";
+        internal const string TiffConstant = "TaggedImageFileFormat";
+        internal const string OemTextConstant = "OEMText";
+        internal const string PaletteConstant = "Palette";
+        internal const string PenDataConstant = "PenData";
+        internal const string RiffConstant = "RiffAudio";
+        internal const string WaveAudioConstant = "WaveAudio";
+        internal const string FileDropConstant = "FileDrop";
+        internal const string LocaleConstant = "Locale";
+        internal const string HtmlConstant = "HTML Format";
+        internal const string RtfConstant = "Rich Text Format";
+        internal const string CsvConstant = "Csv";
+        internal const string StringConstant = "System.String";
+        internal const string SerializableConstant = "WindowsForms10PersistentObject";
+
+        /// <summary>
+        ///  Specifies the standard ANSI text format.
+        /// </summary>
+        public static readonly string Text = TextConstant;
+
+        /// <summary>
+        ///  Specifies the standard Windows Unicode text format.
+        /// </summary>
+        public static readonly string UnicodeText = UnicodeTextConstant;
+
+        /// <summary>
+        ///  Specifies the Windows Device Independent Bitmap (DIB) format.
+        /// </summary>
+        public static readonly string Dib = DibConstant;
+
+        /// <summary>
+        ///  Specifies a Windows bitmap format.
+        /// </summary>
+        public static readonly string Bitmap = BitmapConstant;
+
+        /// <summary>
+        ///  Specifies the Windows enhanced metafile format.
+        /// </summary>
+        public static readonly string EnhancedMetafile = EmfConstant;
+
+        /// <summary>
+        ///  Specifies the Windows metafile format, which WinForms does not directly use.
+        /// </summary>
+        public static readonly string MetafilePict = WmfConstant;
+
+        /// <summary>
+        ///  Specifies the Windows symbolic link format, which WinForms does not directly use.
+        /// </summary>
+        public static readonly string SymbolicLink = SymbolicLinkConstant;
+
+        /// <summary>
+        ///  Specifies the Windows data interchange format, which WinForms does not directly use.
+        /// </summary>
+        public static readonly string Dif = DifConstant;
+
+        /// <summary>
+        ///  Specifies the Tagged Image File Format (TIFF), which WinForms does not directly use.
+        /// </summary>
+        public static readonly string Tiff = TiffConstant;
+
+        /// <summary>
+        ///  Specifies the standard Windows original equipment manufacturer (OEM) text format.
+        /// </summary>
+        public static readonly string OemText = OemTextConstant;
+
+        /// <summary>
+        ///  Specifies the Windows palette format.
+        /// </summary>
+        public static readonly string Palette = PaletteConstant;
+
+        /// <summary>
+        ///  Specifies the Windows pen data format, which consists of pen strokes for handwriting
+        ///  software; WinForms does not use this format.
+        /// </summary>
+        public static readonly string PenData = PenDataConstant;
+
+        /// <summary>
+        ///  Specifies the Resource Interchange File Format (RIFF) audio format, which WinForms does not directly use.
+        /// </summary>
+        public static readonly string Riff = RiffConstant;
+
+        /// <summary>
+        ///  Specifies the wave audio format, which Win Forms does not directly use.
+        /// </summary>
+        public static readonly string WaveAudio = WaveAudioConstant;
+
+        /// <summary>
+        ///  Specifies the Windows file drop format, which WinForms does not directly use.
+        /// </summary>
+        public static readonly string FileDrop = FileDropConstant;
+
+        /// <summary>
+        ///  Specifies the Windows culture format, which WinForms does not directly use.
+        /// </summary>
+        public static readonly string Locale = LocaleConstant;
+
+        /// <summary>
+        ///  Specifies text consisting of HTML data.
+        /// </summary>
+        public static readonly string Html = HtmlConstant;
+
+        /// <summary>
+        ///  Specifies text consisting of Rich Text Format (RTF) data.
+        /// </summary>
+        public static readonly string Rtf = RtfConstant;
+
+        /// <summary>
+        ///  Specifies a comma-separated value (CSV) format, which is a common interchange format
+        ///  used by spreadsheets. This format is not used directly by WinForms.
+        /// </summary>
+        public static readonly string CommaSeparatedValue = CsvConstant;
+
+        /// <summary>
+        ///  Specifies the WinForms string class format, which WinForms uses to store string objects.
+        /// </summary>
+        public static readonly string StringFormat = StringConstant;
+
+        /// <summary>
+        ///  Specifies a format that encapsulates any type of WinForms object.
+        /// </summary>
+        public static readonly string Serializable = SerializableConstant;
+    }
+}
