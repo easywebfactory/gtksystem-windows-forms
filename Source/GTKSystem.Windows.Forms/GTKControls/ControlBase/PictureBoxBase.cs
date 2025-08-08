a1c0b16bb05ad7f6f6406cfbb6b045e9248b8db3
chenhongjin <13632417+easywebfactory@user.noreply.gitee.com>
2025-08-08 11:50:09 +0800

    支持背景色

--- Source/GTKSystem.Windows.Forms/GTKControls/ControlBase/PictureBoxBase.cs
+++ Source/GTKSystem.Windows.Forms/GTKControls/ControlBase/PictureBoxBase.cs
@@ -84,6 +84,7 @@ namespace GTKSystem.Windows.Forms.GTKControls.ControlBase
         
         protected override bool OnDrawn(Cairo.Context cr)
         {
+            bool returndrawn = base.OnDrawn(cr);
             int width = this.AllocatedWidth;
             int height = this.AllocatedHeight;
             Gdk.Rectangle rec = new Gdk.Rectangle(0, 0, width, height);
@@ -100,7 +101,7 @@ namespace GTKSystem.Windows.Forms.GTKControls.ControlBase
 
             Override.OnPaint(cr, rec);
 
-            return base.OnDrawn(cr);
+            return returndrawn;
         }
         private void DrawnSource(Cairo.Context cr, Gdk.Pixbuf image, PictureBoxSizeMode sizeMode, int width,int height)
         {
